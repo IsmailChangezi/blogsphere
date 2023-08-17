49b0a99e4e3afc6702290c2562c683cf13e4e509
Ismail Ali <ismailchangezi801@gmail.com>
2023-08-17 20:24:46 +0500

    Validations and Model Specs: Day 4: Add Validations and Spec for Each Model

--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Comment, type: :model do
     let!(:author) { User.create name: 'John Doe', posts_counter: 0 }
     let!(:post) { Post.create author_id: author.id, title: "God's Creations", comments_counter: 0, likes_counter: 0 }
     let!(:comment) do
-        Comment.create text: "This is a comment", posts_id: 1, author_id: 1
+      Comment.create text: 'This is a comment', posts_id: 1, author_id: 1
     end
 
     it 'should increment comments_counter by one' do
