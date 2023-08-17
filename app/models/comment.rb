106fdaaf057b6e916ecb351556aa4b87039628dd
Ismail Ali <ismailchangezi801@gmail.com>
2023-08-17 12:33:13 +0500

    Processing Data in Models: Day 3: Add Custom Methods For Models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ApplicationRecord
     belongs_to :author, class_name: 'User'
     belongs_to :posts, class_name: "Post"
 
+    after_create :increment_post_comments_counter
+
+    private
+  
+    def increment_post_comments_counter
+      post.update_comments_counter
+    end
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,15 @@ class Post < ApplicationRecord
     has_many :comments, foreign_key: 'posts_id'
     has_many :likes, foreign_key: 'posts_id'
   
+    def update_comments_counter
+      update(comments_count: comments.count)
+    end
+  
+    def recent_comments
+      comments.order(created_at: :desc).limit(5)
+    end
+  
+    def update_likes_counter(new_likes_count)
+      update(likes_count: new_likes_count)
+    end
   end
