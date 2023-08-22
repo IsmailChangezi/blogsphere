39a548564431af05197982ad4f2b55e7e3ab6d23
Ismail Ali <ismailchangezi801@gmail.com>
2023-08-22 15:12:29 +0500

    Controllers Specs - Day:2 Week:2 : Implement Required Changes

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,12 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :request do
-  describe 'GET /users/:user_id/posts/:id' do
-    it 'works! (now write some real specs)' do
-      get '/users/1/posts/1'
+RSpec.describe 'Users', type: :request do
+  let!(:user) { User.create!(name: 'John', posts_counter: 0) }
+
+  describe 'GET /users' do
+    it 'should return a 200 OK status' do
+      get users_path
       expect(response).to have_http_status(200)
-      expect(response).to render_template('posts/show')
-      expect(response.body).to include('Post 1 from User 1')
     end
+
+    it 'should render users/index template' do
+      get users_path
+      expect(response).to render_template('index')
+    end
+  end
+
+  describe 'Get /users/:id' do
+    it 'should return a 200 OK status' do
+      get user_url(user)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should render users/show template' do
+      get user_url(user)
+      expect(response).to render_template('show')
+    end
+
   end
 end
